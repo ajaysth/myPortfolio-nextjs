03fc9a1ba715557dbb40e38e58766b0b65e21032
ajaysth <shrestha11ajay@gmail.com>
2025-09-08 14:32:35 +0545

    header section done

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,18 +1,24 @@
 import { assets } from '@/assets/assets'
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>
+    <div className='w-11/12 max-w-3xl mx-auto h-screen flex flex-col items-center justify-center gap-4'>
         <div>
             <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
             
         </div>
         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I am Ajay Shrestha <Image src={assets.hand_icon} alt='' className='w-6' /></h3>
-        <h1 className='text-3xl sm:text-6xl lg:text-[66px]'>Front end developer in Nepal</h1>
+        <h1 className='text-3xl sm:text-6xl text-center lg:text-[66px]'>Front end developer in Nepal</h1>
         <p className='max-w-2xl mx-auto font-Ovo'>
             I am fullstack developer from Bhaktapur, Nepal.
         </p>
+
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+          <a href="#contact" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>Contact me <Image src={assets.right_arrow_white} alt='' className=' w-4' /></a>
+          <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume <Image src={assets.download_icon} alt='' className=' w-4' /></a> 
+        </div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 'use client'
 import Navbar from "@/app/components/Navbar";
+import Header from "@/app/components/Header";
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 }
