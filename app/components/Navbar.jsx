982f9aa54bf3095430f5544a21cea3b777b98da2
ajaysth <shrestha11ajay@gmail.com>
2025-09-08 14:05:59 +0545

    changes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -26,7 +26,7 @@ const navbar = () => {
         </a>
         <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-60 '>
             <li><a  className= 'font-ovo' href="#top">Home</a></li>
-            <li><a href="#about">About Me</a></li>
+            <li><a href="#about">About me</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#work">My Work</a></li>
             <li><a href="#contact">Contact Me</a></li>
